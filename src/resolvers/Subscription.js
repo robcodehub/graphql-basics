c4e118382e6114646d3871f615a596e5feeb93d1
Robert Wise <robcodehub@gmail.com>
2020-07-28 10:47:44 +1000

    added subscription for comments

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,18 +1,31 @@
 const Subscription = {
-    count: {
-        subscribe(parent, args, { pubsub }, info) {
-            let count = 0
+  count: {
+    subscribe(parent, args, { pubsub }, info) {
+      let count = 0;
 
-            setInterval(() => {
-                count++
-                pubsub.publish('count', {
-                    count
-                })
-            }, 1000)
+      setInterval(() => {
+        count++;
+        pubsub.publish('count', {
+          count,
+        });
+      }, 1000);
 
-            return pubsub.asyncIterator('count')
-        }
-    }
-}
+      return pubsub.asyncIterator('count');
+    },
+  },
+  comment: {
+    subscribe(parent, { postId }, { db, pubsub }, info) {
+      const post = db.posts.find(
+        (post) => post.id === postId && post.published
+      );
 
-export { Subscription as default }
+      if (!post) {
+        throw new Error('Post not found');
+      }
+
+      return pubsub.asyncIterator(`comment ${postId}`);
+    },
+  },
+};
+
+export { Subscription as default };
